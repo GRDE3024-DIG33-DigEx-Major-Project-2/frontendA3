4dc818722b404d873d61e87beceed4e63c262cb6
Jacopo G <S3876518@student.rmit.edu.au>
2023-07-03 14:27:31 +1000

    Header progress

--- src/components/FindEventHeader.js
+++ src/components/FindEventHeader.js
@@ -1,26 +1,30 @@
 import { useState } from "react";
 import { DatePicker } from "@mui/x-date-pickers";
-import { InputLabel, MenuItem, Select } from "@mui/material";
+import { MenuItem, Select, Chip } from "@mui/material";
 
 const FindEventHeader = () => {
 
-    const [location, setLocation] = useState("Sydney");
+    const [location, setLocation] = useState("-");
     const [date, setDate] = useState(null);
+    const [genre, setGenre] = useState("country");
+    const [selected, setSelected] = useState(false);
 
     const searchHandler = () => {
         console.log("Search event fired");
-        console.log(location, date);
+        console.log(location, date, genre);
+    }
+
+    const chipHandler = (genre) => {
+        setGenre(genre);  
+        setSelected((s) => !s);
     }
 
     return <div className="find-event-header">
         <h1>Find an event</h1>
-        <form onSubmit={searchHandler}>
+        <form id="search-event-form" onSubmit={searchHandler}>
             <div className="find-event-search">
-                <InputLabel id="search-locations">Your Location</InputLabel>
                 <Select labelId="search-locations" value={location} label="Your Location" onChange={(event) => setLocation(event.target.value)}>
-                    <MenuItem value="">
-                        <em>None</em>
-                    </MenuItem>
+                    <MenuItem disabled selected hidden value="-">Your Location</MenuItem>
                     <MenuItem value="Sydney">Sydney</MenuItem>
                     <MenuItem value="Balmain">Balmain</MenuItem>
                     <MenuItem value="Surry Hills">Surry Hills</MenuItem>
@@ -30,7 +34,15 @@ const FindEventHeader = () => {
                 </Select>
                 <DatePicker onChange={(event) => setDate(event.target.value)} />
             </div>
-            <div className="find-event-tags"></div>
+            <div className="find-event-tags">
+                <Chip label="Music" value="Music" color={selected ? "primary" : "default"} onClick={() => chipHandler("Music") } />
+            </div>
+            <input
+            id="search-btn"
+            className="search-btn"
+            type="submit"
+            value="Search"
+            />
         </form>
     </div>
     ;
