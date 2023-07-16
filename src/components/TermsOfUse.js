8b889c9f5d9c39bc4eaed88016ff3bf6b449fd51
AshleyS770 <ashley.smith2@virginaustralia.com>
2023-07-16 21:31:17 +1000

    Privacy Policy and Terms of Use pages

--- src/components/TermsOfUse.js
+++ src/components/TermsOfUse.js
@@ -1,7 +1,56 @@
 const TermsOfUse = () => {
     return (
       <>
+      <div className="content-body">
         <h1>Terms of Use</h1>
+        <p>Please read these Terms of Use ("Terms") carefully before using our website ("the Website"). By accessing or using the Website, you agree to be bound by these Terms. If you do not agree with any part of these Terms, you must not use the Website. </p>
+
+        <p>1. Use of the Website</p>
+
+        <p>1.1. The Website provides a platform for users to advertise and promote their events. You may use the Website solely for lawful purposes and in accordance with these Terms. </p>
+
+        <p>1.2. You must be at least 18 years old to use the Website. By using the Website, you represent and warrant that you are 18 years of age or older. </p>
+
+        <p>1.3. You are responsible for maintaining the confidentiality of your account and password and for restricting access to your account. You agree to accept responsibility for all activities that occur under your account. </p>
+
+        <p>2. User Content </p>
+
+        <p>2.1. By submitting or posting content on the Website ("User Content"), you grant us a non-exclusive, royalty-free, perpetual, irrevocable, and fully sublicensable right to use, reproduce, modify, adapt, publish, translate, create derivative works from, distribute, and display such User Content throughout the world in any media. </p>
+
+        <p>2.2. You represent and warrant that you own or have the necessary rights, licenses, and permissions to grant the rights to your User Content as described in these Terms, and that the User Content does not infringe or violate any third party's rights. </p>
+
+        <p>2.3. We reserve the right to remove or modify any User Content that violates these Terms or is otherwise objectionable in our sole discretion. </p>
+
+        <p>3. Intellectual Property Rights </p>
+
+        <p>3.1. The Website and its original content, features, and functionality are owned by us and are protected by international copyright, trademark, patent, trade secret, and other intellectual property or proprietary rights laws. </p>
+
+        <p>3.2. You may not modify, reproduce, distribute, create derivative works or adaptations of, publicly display or in any way exploit any of the content, features, or functionality available on the Website without our prior written consent. </p>
+
+        <p>4. Third-Party Websites and Content </p>
+
+        <p>4.1. The Website may contain links to third-party websites or resources. We do not endorse and are not responsible for the availability, accuracy, or content of such external sites or resources. </p>
+
+        <p>4.2. Any content, products, or services available through third-party websites or resources are subject to the terms and conditions of the respective third parties, and you are solely responsible for reviewing and complying with those terms and conditions. </p>
+
+        <p>5. Limitation of Liability </p>
+
+        <p>5.1. To the fullest extent permitted by applicable law, we shall not be liable for any indirect, incidental, special, consequential, or punitive damages, or any loss of profits or revenues, whether incurred directly or indirectly, or any loss of data, use, goodwill, or other intangible losses, resulting from your access to or use of the Website. </p>
+
+        <p>6. Indemnification </p>
+
+        <p>.1. You agree to indemnify and hold us harmless from and against any claims, liabilities, damages, judgments, awards, losses, costs, expenses, or fees (including reasonable attorneys' fees) arising out of or relating to your violation of these Terms or your use of the Website. </p>
+
+        <p>7. Modification and Termination </p>
+
+        <p>7.1. We reserve the right, in our sole discretion, to modify or terminate the Website or these Terms at any time without prior notice. </p>
+
+        <p>7.2. If any provision of these Terms is found to be unlawful, void, or unenforceable, that provision shall be deemed severable and shall not affect the validity and enforceability of any remaining provisions. </p>
+
+        <p>8. Governing Law and Jurisdiction </p>
+
+        <p>8.1. These Terms shall be governed by and construed in accordance with the laws of The NSW Government. Any legal actions, suits, or proceedings arising out of or relating to these Terms or the Website shall be exclusively brought in the courts of The NSW Government. </p>
+        </div>
       </>
     );
   };
