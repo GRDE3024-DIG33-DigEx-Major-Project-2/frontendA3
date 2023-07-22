17ee2b283a43c58450eb345725461cf14efa0726
Jacopo G <S3876518@student.rmit.edu.au>
2023-07-22 10:35:48 +0930

    Homepage responnsive form

--- src/components/event/FindEventHeader.js
+++ src/components/event/FindEventHeader.js
@@ -1,10 +1,20 @@
 import { DatePicker } from "@mui/x-date-pickers";
-import { MenuItem, Select, Chip, TextField, Button } from "@mui/material";
+import {
+  MenuItem,
+  Select,
+  Chip,
+  TextField,
+  Button,
+  InputLabel,
+  FormControl,
+} from "@mui/material";
 import { getAllTags } from "../../utils/utils";
 import { useState, useEffect } from "react";
+import FmdGoodOutlinedIcon from '@mui/icons-material/FmdGoodOutlined';
+import InputAdornment from "@mui/material/InputAdornment";
 
 const FindEventHeader = () => {
-  const [location, setLocation] = useState("-");
+  const [location, setLocation] = useState("");
   const [date, setDate] = useState(null);
   const [genre, setGenre] = useState("country");
   const [tags, setTags] = useState([]);
@@ -32,42 +42,45 @@ const FindEventHeader = () => {
       <h1 className="find-event-header-text">Find an event</h1>
       <form id="search-event-form" onSubmit={searchHandler}>
         <div className="find-event-search">
-          <Select
-            className="search-form-els"
-            labelId="search-locations"
-            value={location}
-            label="Your Location"
-            onChange={(event) => setLocation(event.target.value)}
-          >
-            <MenuItem disabled selected hidden value="-">
-              Your Location
-            </MenuItem>
-            <MenuItem value="Sydney">Sydney</MenuItem>
-            <MenuItem value="Balmain">Balmain</MenuItem>
-            <MenuItem value="Surry Hills">Surry Hills</MenuItem>
-            <MenuItem value="Parramatta">Parramatta</MenuItem>
-            <MenuItem value="Marrickville">Marrickville</MenuItem>
-            <MenuItem value="Lane Cove">Lane Cove</MenuItem>
-          </Select>
+          <FormControl fullWidth>
+            <InputLabel id="location-label"><FmdGoodOutlinedIcon /> My location</InputLabel>
+            <Select
+              className="search-form-els"
+              labelId="location-label"
+              label="My Location"
+              value={location}
+              onChange={(event) => setLocation(event.target.value)}
+            >
+              {/* <MenuItem disabled selected hidden value="-">
+              My Location
+            </MenuItem> */}
+              <MenuItem value="Sydney">Sydney</MenuItem>
+              <MenuItem value="Balmain">Balmain</MenuItem>
+              <MenuItem value="Surry Hills">Surry Hills</MenuItem>
+              <MenuItem value="Parramatta">Parramatta</MenuItem>
+              <MenuItem value="Marrickville">Marrickville</MenuItem>
+              <MenuItem value="Lane Cove">Lane Cove</MenuItem>
+            </Select>
+          </FormControl>
+
           <DatePicker
             className="search-form-els"
             onChange={(event) => setDate(event.target.value)}
           />
-          <span>
-            <TextField
-              className="search-form-els"
-              id="events-txt-field"
-              variant="outlined"
-              label="Search artists, venues or events"
-            ></TextField>
-            <Button
-              className="search-form-els"
-              type="submit"
-              variant="contained"
-            >
-              Search
-            </Button>
-          </span>
+          <TextField
+            className="search-form-els"
+            id="events-txt-field"
+            variant="outlined"
+            label="Search artists, venues or events"
+          ></TextField>
+          <Button
+            className="search-form-els"
+            id="search-form-submit-btn"
+            type="submit"
+            variant="contained"
+          >
+            Search
+          </Button>
         </div>
         <div className="find-event-tags">
           {tags.map((tag, i) => (
@@ -75,7 +88,7 @@ const FindEventHeader = () => {
               sx={{
                 backgroundColor: "#7759A6",
                 color: "white",
-                margin: "1%"
+                margin: "1%",
               }}
               key={i}
               label={tag.name}
