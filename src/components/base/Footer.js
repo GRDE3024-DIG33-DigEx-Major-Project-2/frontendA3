2d6b5db279431ee5a3418fd218dc282c3195e554
Jacopo G <S3876518@student.rmit.edu.au>
2023-07-22 09:35:40 +0930

    mobile menu implementation
    responsive mobile menu

--- src/components/base/Footer.js
+++ src/components/base/Footer.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
     const year = new Date().getFullYear();
-  
+
     return <footer>
       {`© ${year}. Team X Inc. All rights reserved`} | <Link id="footer-tou" to="../terms-of-use">Terms of Use</Link> | <Link id="footer-privpol" to="../privacy-policy">Privacy Policy</Link>
     </footer>;
--- src/components/base/Header.js
+++ src/components/base/Header.js
@@ -1,14 +1,42 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Drawer, IconButton } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
+import { createTheme } from "@mui/material/styles";
+import { ThemeProvider } from "@mui/material/styles";
 
 export default function Header({ user, isLoggedIn, setIsLoggedIn, setUser }) {
+  const [menu, setMenu] = useState(false);
+  const year = new Date().getFullYear();
+
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#ffffff",
+      },
+      neutral: {
+        main: "#e6e2e2;",
+      },
+    },
+  });
+
   const logOut = () => {
     setIsLoggedIn(false);
+    toggleDrawer(false);
     setUser(null);
   };
 
   // only for mobile menu
-  const openMenu = () => {
-    console.log("opening menu");
+  const toggleDrawer = (menu) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+    //changes the function state according to the value of open
+    setMenu(menu);
   };
 
   return (
@@ -24,7 +52,7 @@ export default function Header({ user, isLoggedIn, setIsLoggedIn, setUser }) {
         </Link>
         <div className="nav-links">
           <Link id="nav-events" to="/events">
-            Events
+            Search
           </Link>
           {isLoggedIn && user.type === "guest" && (
             <Link id="nav-profile" to="/profile">
@@ -33,7 +61,11 @@ export default function Header({ user, isLoggedIn, setIsLoggedIn, setUser }) {
           )}
           {isLoggedIn && user.type === "organiser" && (
             <>
-              <Link id="nav-create-event" to="/create-event">
+              <Link
+                id="nav-create-event"
+                className="bttn-style-orange"
+                to="/create-event"
+              >
                 Create Event
               </Link>
               <Link id="nav-dashboard" to="/dashboard">
@@ -43,24 +75,141 @@ export default function Header({ user, isLoggedIn, setIsLoggedIn, setUser }) {
           )}
           {!isLoggedIn && (
             <>
-              <Link id="nav-signup" to="/signup">
+              <Link id="nav-signup" className="bttn-style-purple" to="/signup">
                 Signup
               </Link>
-              <Link id="nav-login" to="/login">
+              <Link id="nav-login" className="bttn-style-orange" to="/login">
                 Login
               </Link>
             </>
           )}
           {isLoggedIn && (
-            <Link id="nav-logout" to="../" onClick={logOut}>
+            <Link
+              id="nav-logout"
+              className="bttn-style-purple"
+              to="../"
+              onClick={logOut}
+            >
               Logout
             </Link>
           )}
         </div>
+
+        {/* MOBILE MENU */}
         <div id="mobile-menu">
-          <Link id="mobile-menu-link" onClick={openMenu}>
-            Menu
-          </Link>
+          <ThemeProvider theme={theme}>
+            <IconButton
+              color="primary"
+              edge="start"
+              aria-label="open menu"
+              onClick={toggleDrawer(true)}
+              sx={{
+                mr: 1,
+                display: {
+                  xs: "block",
+                  sm: "none",
+                },
+              }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Drawer
+              id="menu-drawer"
+              anchor="right"
+              open={menu}
+              onClose={toggleDrawer(false)}
+              onOpen={toggleDrawer(true)}
+            >
+              <div className="mobile-menu-box">
+                <CloseIcon
+                  size="large"
+                  color="primary"
+                  className="close-icon-btn"
+                  fontSize="2"
+                  sx={{ ml: "75%", mb: "155%" }}
+                  onClick={toggleDrawer(false)}
+                />
+                <div id="gigney-menu-logo">
+                  <img src="../gigney-logo-vertical.png" alt="gigney logo" />
+                </div>
+                <h1>Menu</h1>
+
+                {isLoggedIn && (
+                  <>
+                    <div id="loggedin-links-menu">
+                      <Link
+                        className="mobile-menu-link"
+                        to="/home"
+                        onClick={toggleDrawer(false)}
+                      >
+                        Home
+                      </Link>
+                      <Link
+                        className="mobile-menu-link"
+                        to="/events"
+                        onClick={toggleDrawer(false)}
+                      >
+                        Search
+                      </Link>
+                      <Link
+                        className="mobile-menu-link"
+                        to="/profile"
+                        onClick={toggleDrawer(false)}
+                      >
+                        Profile
+                      </Link>
+                    </div>
+                    <Link
+                      className="bttn-style-purple"
+                      to="../"
+                      onClick={logOut}
+                    >
+                      Logout
+                    </Link>
+                  </>
+                )}
+                {!isLoggedIn && (
+                  <div id="loggedout-bttns-menu">
+                    <Link
+                      id="nav-signup"
+                      className="bttn-style-purple"
+                      to="/signup"
+                      onClick={toggleDrawer(false)}
+                    >
+                      Signup
+                    </Link>
+                    <Link
+                      id="nav-login"
+                      className="bttn-style-orange"
+                      to="/login"
+                      onClick={toggleDrawer(false)}
+                    >
+                      Login
+                    </Link>
+                  </div>
+                )}
+              </div>
+              <div className="menu-footer">
+                {`© ${year}. Team X Inc. All rights reserved`} |{" "}
+                <Link
+                  id="footer-tou"
+                  to="../terms-of-use"
+                  onClick={toggleDrawer(false)}
+                >
+                  Terms of Use
+                </Link>{" "}
+                |{" "}
+                <Link
+                  id="footer-privpol"
+                  to="../privacy-policy"
+                  onClick={toggleDrawer(false)}
+                >
+                  Privacy Policy
+                </Link>
+              </div>
+              ;
+            </Drawer>
+          </ThemeProvider>
         </div>
       </nav>
     </header>
