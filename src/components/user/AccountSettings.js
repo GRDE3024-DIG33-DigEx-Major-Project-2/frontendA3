f7ce97096359b723c1ad1c5d143ce12de142902c
Jacopo G <S3876518@student.rmit.edu.au>
2023-08-12 08:31:55 +1000

    Delete account fix (added modal to match mock up)

--- src/components/user/AccountSettings.js
+++ src/components/user/AccountSettings.js
@@ -10,70 +10,120 @@ import {
   FormControl,
   TextField,
   InputAdornment,
+  Modal,
 } from "@mui/material";
 import ResetPassword from "./ResetPassword";
 import { PATHS } from "../../utils/constants.util";
 import { LoadingContext } from "../../props/loading-spinner.prop";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { deleteUser } from "../../services/UserAPI";
 import { showSuccessToast, showToast, showErrorToast } from "../shared/Toaster";
 import { logoutErrorHandler } from "../../services/AuthAPI";
 
 const AccountSettings = () => {
+  //Loading spinner props
+  const { loading, setLoading } = useContext(LoadingContext);
 
-      //Loading spinner props
-      const {
-        loading,
-        setLoading
-      } = useContext(LoadingContext);
-  
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
+  const [modalOpen, setModalOpen] = useState(false);
+  const [confirmationModalOpen, setConfirmationModalOpen] = useState(false);
 
-    /**
-     * Deletes the user's account and then logs them out
-     */
+  // Modal functions
+  const handleModalOpen = () => setModalOpen(true);
+  const handleModalClose = () => setModalOpen(false);
+  const handleConfirmationModalOpen = () => setConfirmationModalOpen(true);
+  const handleConfirmationModalClose = () => setConfirmationModalOpen(false);
+
+  /**
+   * Deletes the user's account and then logs them out
+   */
   const handleDelete = async (e) => {
-
     e.preventDefault();
     console.log("...deleting account");
 
-    const userConfirmation = window.confirm("Are you sure you want to delete your account?");
-    if (userConfirmation) {
-    try {
-      setLoading(true);
+    // const userConfirmation = window.confirm(
+    //   "Are you sure you want to delete your account?"
+    // );
+    // if (userConfirmation) {
+      try {
+        setLoading(true);
 
-      let response = await deleteUser();
-
-    navigate(PATHS.LOGIN);        
-    }
-    catch(error) {
-      console.log(error);
-    }
-    finally {
-      setLoading(false);
-    }      
-    }
-  }
-
-
-    return (
-      <>
-                    <h2>Account settings</h2>
-                    <Box className="profile-box prof-left">
-    <FormControl fullWidth>
-<ResetPassword></ResetPassword>     
-        <Link
-        id="delete-account-profile"
-        to={PATHS.HOME}
-        onClick={handleDelete}
-      >
-        Delete this account
-      </Link>     
-    </FormControl>
-  </Box>      
-      </>
-    );
+        let response = await deleteUser();
+        handleConfirmationModalOpen();
+        handleModalClose();
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    // }
   };
-  
-  export default AccountSettings;
+
+  return (
+    <>
+      <h2>Account settings</h2>
+      <Box className="profile-box prof-left">
+        <FormControl fullWidth>
+          <ResetPassword></ResetPassword>
+          <Link
+            id="delete-account-profile"
+            onClick={handleModalOpen}
+          >
+            Delete this account
+          </Link>
+        </FormControl>
+      </Box>
+      <Modal
+        open={modalOpen}
+        onClose={handleModalClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box className="delete-event-modal">
+          <h2>Are you sure you want to delete your Gigney account?</h2>
+          <span>
+            All your data will be removed and permanently deleted, so you will
+            not be able to retrieve any of your existing information.
+          </span>
+          <div>
+            <Button
+              id="save-exit-ev-btn"
+              variant="contained"
+              className="input-btn"
+              onClick={handleModalClose}
+            >
+              No, I've changed my mind
+            </Button>
+            <Button
+              id="save-cont-ev-btn"
+              variant="contained"
+              onClick={handleDelete}
+            >
+              Yes, delete my account
+            </Button>
+          </div>
+        </Box>
+      </Modal>
+      <Modal
+        open={confirmationModalOpen}
+        onClose={handleConfirmationModalClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+        id="confirmation-modal"
+      >
+        <Box className="delete-event-modal">
+          <h2>Success!</h2>
+          <span>
+            Your account and all its data has been permanently deleted.
+          </span>
+          <Button id="save-cont-ev-btn" variant="contained" href="/login">
+            Sign out
+          </Button>
+        </Box>
+      </Modal>
+    </>
+  );
+};
+
+export default AccountSettings;
--- src/services/EventAPI.js
+++ src/services/EventAPI.js
@@ -3,19 +3,15 @@
  */
 
 //Import dependencies
-import {
-  EVENT_ENDPOINTS,
-} from "../utils/constants.util";
+import { EVENT_ENDPOINTS } from "../utils/constants.util";
 import { getAccessToken } from "../utils/localStorage";
 import { logoutErrorHandler } from "./AuthAPI";
 import axiosClient from "./Axios";
 
-
-
 /**
  * Get a page of events from api endpoint
- * @param {*} tagIds 
- * @param {*} keywords Keywords that match 
+ * @param {*} tagIds
+ * @param {*} keywords Keywords that match
  * @param {*} minDate The minimum date for an event
  * @param {*} maxDate The maximum date for an event
  * @param {*} city The city the event is in
@@ -23,28 +19,34 @@ import axiosClient from "./Axios";
  * @param {*} page The page of event matches requested
  * @returns Array of events and number of pages that match the filter options
  */
-export const searchEvents = async function (tagIds, keywords, minDate, maxDate, city, priceRange, page) {
-
+export const searchEvents = async function (
+  tagIds,
+  keywords,
+  minDate,
+  maxDate,
+  city,
+  priceRange,
+  page
+) {
   //The sanitized price range for request body
   let priceSetting = null;
 
   //Sanitize price range for endpoint validation
   if (priceRange != null) {
-    if (priceRange.minPrice !== null
-      && priceRange.maxPrice !== null
-      && priceRange.minPrice !== 0
-      && priceRange.maxPrice !== 0)
+    if (
+      priceRange.minPrice !== null &&
+      priceRange.maxPrice !== null &&
+      priceRange.minPrice !== 0 &&
+      priceRange.maxPrice !== 0
+    )
       priceSetting = {
         minPrice: priceRange.minPrice,
-        maxPrice: priceRange.maxPrice
+        maxPrice: priceRange.maxPrice,
       };
   }
 
-
   //Sanitize keywords by setting empty keywords string to null
-  if (keywords === "")
-    keywords = null;
-
+  if (keywords === "") keywords = null;
 
   //The array of events to return
   let events = [];
@@ -59,7 +61,7 @@ export const searchEvents = async function (tagIds, keywords, minDate, maxDate, 
     maxDate: maxDate,
     city: city,
     priceRange: priceSetting,
-    page: page | 0
+    page: page | 0,
   };
 
   try {
@@ -70,8 +72,7 @@ export const searchEvents = async function (tagIds, keywords, minDate, maxDate, 
         events = response.data.events;
         pageCount = response.data.pageCount;
       });
-  }
-  catch (error) {
+  } catch (error) {
     console.log("An error occured while searching events!");
     console.log(tagIds, keywords, minDate, maxDate, city, page);
     //Request body is invalid!
@@ -79,15 +80,11 @@ export const searchEvents = async function (tagIds, keywords, minDate, maxDate, 
       console.log("Request body is invalid!");
       console.log(error.response.data.errors);
     }
-  }
-  finally {
+  } finally {
     //Return object containing events and total page count
     return { events: events, pageCount: pageCount };
   }
-
-
 };
-
 
 /**
  * Get an event by id
@@ -95,10 +92,8 @@ export const searchEvents = async function (tagIds, keywords, minDate, maxDate, 
  * @returns The event's data
  */
 export const getEventById = async function (id) {
-
   try {
-    let response = await axiosClient
-      .get(EVENT_ENDPOINTS.getByIdUrl + `/${id}`);
+    let response = await axiosClient.get(EVENT_ENDPOINTS.getByIdUrl + `/${id}`);
 
     //Success!
     if (response.status == 200) {
@@ -111,49 +106,43 @@ export const getEventById = async function (id) {
       console.log(response);
       return response;
     }
-  }
-  catch (error) {
+  } catch (error) {
     console.log("Failed to find event by id!");
     console.log(error);
   }
-}
-
+};
 
 /**
  * Deletes the user's owned event by id (must be an Organizer)
  * @param {*} eventId Event to delete
- * @param {*} user 
+ * @param {*} user
  */
 export const deleteEvent = async function (eventId) {
-
   const options = {
     headers: {
       Authorization: `Bearer ${getAccessToken()}`,
     },
   };
-try {
-  let response = await axiosClient
-    .delete(EVENT_ENDPOINTS.deleteEventUrl + `/${eventId}`, options)
-    .catch((error) => logoutErrorHandler(error));
+  try {
+    let response = await axiosClient
+      .delete(EVENT_ENDPOINTS.deleteEventUrl + `/${eventId}`, options)
+      .catch((error) => logoutErrorHandler(error));
 
-  //Success!
-  if (response.status === 200) {
-    console.log("Event deleted!");
-    return response;
-  }
-  catch (error) {
+    //Success!
+    if (response.status === 200) {
+      console.log("Event deleted!");
+      return response;
+    }
+  } catch (error) {
     logoutErrorHandler(error);
   }
 };
-
-
 
 /**
  * Find all tags in the database
  * @returns Object Array of Tags
  */
 export const getAllTags = async function () {
-
   let tags = [];
 
   await axiosClient
@@ -168,15 +157,12 @@ export const getAllTags = async function () {
   return tags;
 };
 
-
-
 /**
  * Get a page of favourited events from api endpoint
  * @param {*} page
  * @returns Array of events and number of pages that match the filter options
  */
 export const searchOwnedEvents = async function (page) {
-
   console.log("Searching for page ", page, " of owned events");
 
   const options = {
@@ -198,32 +184,31 @@ export const searchOwnedEvents = async function (page) {
   console.log("Owned events request body: ", requestBody);
   try {
     //Get the array of events and the page number
-    let response = await axiosClient
-      .post(EVENT_ENDPOINTS.searchOwnedEventsUrl, requestBody, options);
+    let response = await axiosClient.post(
+      EVENT_ENDPOINTS.searchOwnedEventsUrl,
+      requestBody,
+      options
+    );
 
-  //Success!
-  if (response.status === 200) {
-    console.log("Owned events search completed!");
-    events = response.data.events;
-    pageCount = response.data.pageCount;
-  }
-  //Failed!
-  else {
-    console.log("Error while searching owned events");
-    console.log(response.status);
-    console.log(response);
-  }    
-  }
-  catch (error) {
+    //Success!
+    if (response.status === 200) {
+      console.log("Owned events search completed!");
+      events = response.data.events;
+      pageCount = response.data.pageCount;
+    }
+    //Failed!
+    else {
+      console.log("Error while searching owned events");
+      console.log(response.status);
+      console.log(response);
+    }
+  } catch (error) {
     logoutErrorHandler(error);
   }
-
 
   //Return object containing API response data
   return { events: events, pageCount: pageCount };
 };
-
-
 
 /**
  * Create event via API POST request
@@ -233,7 +218,6 @@ export const searchOwnedEvents = async function (page) {
  * @returns Event creation result
  */
 export const createEvent = async function (formData) {
-
   //The created event
   let createdEvent = null;
 
@@ -250,27 +234,25 @@ export const createEvent = async function (formData) {
       Authorization: `Bearer ${getAccessToken()}`,
     },
   };
-try {
-  //Perform first event create request
-  let response = await axiosClient.post(
-    EVENT_ENDPOINTS.createEventUrl,
-    formData,
-    createEventOptions
-  );
+  try {
+    //Perform first event create request
+    let response = await axiosClient.post(
+      EVENT_ENDPOINTS.createEventUrl,
+      formData,
+      createEventOptions
+    );
 
-  //Success!
-  if (response.status === 201) {
-    console.log("Create Event Success!");
-    createdEvent = response.data;
-  }
-  catch (error) {
+    //Success!
+    if (response.status === 201) {
+      console.log("Create Event Success!");
+      createdEvent = response.data;
+    }
+  } catch (error) {
     logoutErrorHandler(error);
   }
   //Return object containing API response data
   return createdEvent;
 };
-
-
 
 /**
  * Get a page of favourited events from api endpoint
@@ -297,32 +279,32 @@ export const searchFavourites = async function (page) {
   };
 
   console.log("Favourited events request body: ", requestBody);
-try {
-  //Get the array of favourited events and the page number
-  let response = await axiosClient
-    .post(EVENT_ENDPOINTS.searchFavouritesUrl, requestBody, options);
+  try {
+    //Get the array of favourited events and the page number
+    let response = await axiosClient.post(
+      EVENT_ENDPOINTS.searchFavouritesUrl,
+      requestBody,
+      options
+    );
 
-  //Success!
-  if (response.status === 200) {
-    console.log("Favourited events search completed!");
-    events = response.data.events;
-    pageCount = response.data.pageCount;
-  }
-  catch (error) {
+    //Success!
+    if (response.status === 200) {
+      console.log("Favourited events search completed!");
+      events = response.data.events;
+      pageCount = response.data.pageCount;
+    }
+  } catch (error) {
     logoutErrorHandler(error);
   }
   //Return object containing API response data
   return { events: events, pageCount: pageCount };
-
 };
-
 
 /** Toggles an event as favourited/unfavourited for an Attendee
  * @param {*} eventId
  */
 
 export const toggleFavourite = async function (eventId) {
-
   const options = {
     headers: {
       Authorization: `Bearer ${getAccessToken()}`,
@@ -332,16 +314,19 @@ export const toggleFavourite = async function (eventId) {
   const requestBody = {
     eventId: eventId,
   };
-try {
-  let response = await axiosClient
-    .post(EVENT_ENDPOINTS.toggleFavouriteUrl, requestBody, options);
+  try {
+    let response = await axiosClient.post(
+      EVENT_ENDPOINTS.toggleFavouriteUrl,
+      requestBody,
+      options
+    );
 
-  //Success!
-  if (response.status === 200) {
-    console.log("Event favourited toggled!");
-    return response;
-  }
-  catch (error) {
+    //Success!
+    if (response.status === 200) {
+      console.log("Event favourited toggled!");
+      return response;
+    }
+  } catch (error) {
     logoutErrorHandler(error);
   }
 };
@@ -352,40 +337,38 @@ try {
  * @returns {[]} Array of objects with eventId and isFavourite props
  */
 export const isFavourited = async function (eventIds) {
-
   //Array of objects with eventId and isFavourite props
   let favStatuses = [];
 
   console.log("Inside isFavourited endpoint handler");
 
   let requestBody = {
-    eventIds: eventIds
+    eventIds: eventIds,
   };
-
 
   const options = {
     headers: {
       Authorization: `Bearer ${getAccessToken()}`,
     },
   };
-try {
-  let response = await axiosClient
-    .put(EVENT_ENDPOINTS.isFavourited, requestBody, options);
+  try {
+    let response = await axiosClient.put(
+      EVENT_ENDPOINTS.isFavourited,
+      requestBody,
+      options
+    );
 
-  //Success!
-  if (response.status === 200) {
-    console.log("isFavourited Success!");
-    favStatuses.push(response.data.favStatuses);
-  }
-  catch (error) {
+    //Success!
+    if (response.status === 200) {
+      console.log("isFavourited Success!");
+      favStatuses.push(response.data.favStatuses);
+    }
+  } catch (error) {
     logoutErrorHandler(error);
   }
   //Return the array of objects with eventId and isFavourite props
   return favStatuses;
-
 };
-
-
 
 /**
  * Update event via API PUT request
@@ -393,7 +376,6 @@ try {
  * @returns Event update result
  */
 export const updateEvent = async function (formData) {
-
   console.log("Inside updateEvent");
   console.log(formData);
 
@@ -401,24 +383,24 @@ export const updateEvent = async function (formData) {
   const updateEventOptions = {
     //Set to multipart/form-data
     headers: {
-      'Content-Type': 'multipart/form-data',
+      "Content-Type": "multipart/form-data",
       Authorization: `Bearer ${getAccessToken()}`,
     },
   };
-try {
-  //Perform first event update request
-  let response = await axiosClient
-    .put(EVENT_ENDPOINTS.updateEventUrl, formData, updateEventOptions)
-    .catch((error) => logoutErrorHandler(error));
+  try {
+    //Perform first event update request
+    let response = await axiosClient
+      .put(EVENT_ENDPOINTS.updateEventUrl, formData, updateEventOptions)
+      .catch((error) => logoutErrorHandler(error));
 
-  console.log("Performed first event update request");
+    console.log("Performed first event update request");
 
-  //Success!
-  if (response === 200) {
-    console.log("Update Event Success!");
-    return response.data;
-  }
-  catch (error) {
+    //Success!
+    if (response === 200) {
+      console.log("Update Event Success!");
+      return response.data;
+    }
+  } catch (error) {
     logoutErrorHandler(error);
   }
 };
