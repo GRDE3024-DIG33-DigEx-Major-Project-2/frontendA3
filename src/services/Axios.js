455a4f6fffc00a6926476b12e0fb7537d9136a8a
Jacopo G <S3876518@student.rmit.edu.au>
2023-08-15 18:18:34 +1000

    Task 10. fixed search result display and buttons

--- src/services/Axios.js
+++ src/services/Axios.js
@@ -3,73 +3,103 @@
  */
 
 //Import dependencies
-import axios from 'axios';
-import { AUTH_ENDPOINTS, PATHS } from '../utils/constants.util';
-import { getAccessToken, setAccessToken, resetTokenSession, resetUserSession } from '../utils/localStorage';
-import { showErrorToast, showSuccessToast, showToast } from '../components/shared/Toaster';
+import axios from "axios";
+import { AUTH_ENDPOINTS, PATHS } from "../utils/constants.util";
+import {
+  getAccessToken,
+  setAccessToken,
+  resetTokenSession,
+  resetUserSession,
+} from "../utils/localStorage";
+import {
+  showErrorToast,
+  showSuccessToast,
+  showToast,
+} from "../components/shared/Toaster";
 
 //Init Axios instance
 const axiosClient = axios.create();
 
 export const setupAxiosInterceptors = (navigate, setIsLoggedIn) => {
-
-
   //Interceptor for refreshing JWT token
   axiosClient.interceptors.response.use(
     async (response) => {
-        console.log("Interceptor: Response onfulfilled");
-        return response;
+      console.log("Interceptor: Response onfulfilled");
+      return response;
     },
     async (error) => {
-        console.log("Interceptor: Response onerror");
+      console.log("Interceptor: Response onerror");
+      const originalRequest = error.config;
 
-        // First, ensure the error has a response property
-        if (!error.response) {
-            return Promise.reject(error);
+      //If the response status is 403 (Forbidden) and it's not the initial token request
+      if (
+        error.response.status === 403 &&
+        !originalRequest._retry &&
+        originalRequest.url !== AUTH_ENDPOINTS.refreshTokenUrl
+      ) {
+        originalRequest._retry = true;
+        console.log(AUTH_ENDPOINTS.refreshTokenUrl);
+        console.log("PASSES CONDITIONAL");
+
+        try {
+          console.log("PASSES CONDITIONAL2");
+          const refreshTokenResponse = await axios
+            .get(AUTH_ENDPOINTS.refreshTokenUrl, {
+              headers: {
+                Authorization: `Bearer ${getAccessToken()}`,
+              },
+            })
+            .catch((error) => {
+              console.log("THIS ERROR CAUGHT");
+              logoutAndRedirect(navigate, setIsLoggedIn);
+            });
+
+          console.log("REFRESH REQ COMPLETE");
+          if (
+            (refreshTokenResponse.status >= 200 &&
+              refreshTokenResponse.status <= 299) ||
+            refreshTokenResponse.status === 400
+          ) {
+            console.log("Token refresh succeeded!");
+            setAccessToken(refreshTokenResponse.data.accessToken);
+
+            //Retry the original request with the new token
+            originalRequest.headers[
+              "Authorization"
+            ] = `Bearer ${refreshTokenResponse.data.accessToken}`;
+
+            //Retry the request and if it fails again, logout
+            return axiosClient(originalRequest).catch((retryError) => {
+              console.log("Retry failed after token refresh: ", retryError);
+              logoutAndRedirect(navigate, setIsLoggedIn);
+            });
+          } else {
+            console.log(
+              "Refresh token responded failed status: ",
+              refreshTokenResponse.status
+            );
+            logoutAndRedirect(navigate, setIsLoggedIn);
+          }
+        } catch (refreshError) {
+          console.log("Error refreshing token:", refreshError);
+          logoutAndRedirect(navigate, setIsLoggedIn);
+          return Promise.resolve();
         }
+      }
 
-        const originalRequest = error.config;
-
-        // If the response status is 403 (Forbidden) and it's not the initial token request
-        if (error.response.status === 403 && !originalRequest._retry && originalRequest.url !== AUTH_ENDPOINTS.refreshTokenUrl) {
-            originalRequest._retry = true;
-            console.log(AUTH_ENDPOINTS.refreshTokenUrl);
-            console.log("PASSES CONDITIONAL");
-
-            try {
-                console.log("PASSES CONDITIONAL2");
-                const refreshTokenResponse = await axios.get(AUTH_ENDPOINTS.refreshTokenUrl, {
-                    headers: {
-                        Authorization: `Bearer ${getAccessToken()}`,
-                    },
-                });
-
-                console.log("REFRESH REQ COMPLETE");
-                
-                if (refreshTokenResponse.status >= 200 && refreshTokenResponse.status <= 299 || refreshTokenResponse.status === 400) {
-                    // ... rest of your code for handling success ...
-                    setAccessToken(refreshTokenResponse.data.accessToken);
-                    originalRequest.headers['Authorization'] = `Bearer ${refreshTokenResponse.data.accessToken}`;
-                    return axiosClient(originalRequest);
-                } else {
-                    throw new Error(`Refresh token responded with failed status: ${refreshTokenResponse.status}`);
-                }
-            } catch (refreshError) {
-                console.log('Error refreshing token:', refreshError);
-                logoutAndRedirect(navigate, setIsLoggedIn);
-                return Promise.resolve(); // Ensuring return after logout
-            }
-        }
-
-        // If it's not a 403 error or it's already been retried, let it propagate normally.
-        return Promise.reject(error);
+      //If it's not a 403 error or it's already been retried, let it propagate normally.
+      return Promise.reject(error);
     }
-);
+  );
+
+  //Return the axios instance
+  return axiosClient;
+};
 
 /**
  * Handles normal logout procedures
- * @param {*} navigate 
- * @param {*} setIsLoggedIn 
+ * @param {*} navigate
+ * @param {*} setIsLoggedIn
  */
 function logoutAndRedirect(navigate, setIsLoggedIn) {
   console.log("In the logout function");
@@ -79,7 +109,6 @@ function logoutAndRedirect(navigate, setIsLoggedIn) {
   showToast("You have been logged out");
   navigate(PATHS.LOGIN);
 }
-
 
 //Export the axios instance as default export
 export default axiosClient;
